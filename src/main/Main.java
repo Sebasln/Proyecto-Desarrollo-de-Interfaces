4f2f9d90b648b3ad75e004786d349026cc395c59
Sebasln <sebastian.silva1.dosa@gmail.com>
2025-12-02 21:11:09 +0100

    prueba 2

--- src/main/Main.java
+++ src/main/Main.java
@@ -14,6 +14,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Soy el main del proyecto");
 		System.out.println("Prueba");
+		System.out.println("Prueba 2");
 	}
 
 	public static void recursosEmail() {
