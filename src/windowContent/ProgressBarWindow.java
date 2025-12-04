68adb82898b18a96c919723fa9f536844ca219a7
Sebasln <sebastian.silva1.dosa@gmail.com>
2025-12-04 18:33:26 +0100

    Prueba push antigravity

--- src/windowContent/ProgressBarWindow.java
+++ src/windowContent/ProgressBarWindow.java
@@ -1,5 +1,17 @@
 package windowContent;
+import javax.swing.JWindow;
+import javax.swing.JProgressBar;
 
-public class ProgressBarWindow {
+public class ProgressBarWindow extends JWindow{
 	/*Aquí irá la barra de carga del inicio del programa*/
+	private JProgressBar progressBar;
+
+	public ProgressBarWindow(JProgressBar progressBar) {
+		this.add(progressBar);
+		this.setSize(300, 50);
+		this.setLocationRelativeTo(null);
+		this.setVisible(true);
+
+
+	}
 }
