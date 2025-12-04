ee0f1254f4ed25e2a7693bf9789f694c81579b18
Sebasln <sebastian.silva1.dosa@gmail.com>
2025-12-04 11:16:35 +0100

    avances clase

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,18 @@
 package main;
 
+import java.io.FileNotFoundException;
+
+import fileLogic.FileManager;
+
 public class Main {
 
 	public static void main(String[] args) {
-		
+		try {
+			FileManager.readUsers();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
